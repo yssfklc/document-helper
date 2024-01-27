209043f428cf1b08c580c9c7ee1881fd1eafaa95
Yusuf <121329421+yssfklc@users.noreply.github.com>
2024-01-27 17:48:03 +0300

    first Commit
    first Commit

---
+++ ingestion.py
@@ -0,0 +1,29 @@
+import os
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Pinecone
+import pinecone
+
+pc = pinecone.Pinecone(
+    PINECONE_API_KEY=os.getenv('PINECONE_API_KEY'),
+    PINECONE_ENVIRONMENT_REGION=os.getenv('PINECONE_ENVIRONMENT_REGION')
+)
+
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="langchain-docs/")
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents) } documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+         chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
+    )
+    documents = text_splitter.split_documents(documents=raw_documents)
+    print(f"loaded {len(raw_documents)} documents")
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(documents, embeddings, index_name="medium-blogs-embeddings-index")
+    print("Added to Pinecone vectorstore")
+
+
+if __name__ == "__main__":
+    ingest_docs()
+
