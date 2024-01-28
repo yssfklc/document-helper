904b05586d24d30eb9960a567310759842ae9696
Yusuf <121329421+yssfklc@users.noreply.github.com>
2024-01-28 16:43:06 +0300

    Implemented user interface

--- ingestion.py
+++ ingestion.py
@@ -7,16 +7,17 @@ import pinecone
 from consts import INDEX_NAME
 
 pc = pinecone.Pinecone(
-    PINECONE_API_KEY=os.getenv('PINECONE_API_KEY'),
-    PINECONE_ENVIRONMENT_REGION=os.getenv('PINECONE_ENVIRONMENT_REGION')
+    PINECONE_API_KEY=os.getenv("PINECONE_API_KEY"),
+    PINECONE_ENVIRONMENT_REGION=os.getenv("PINECONE_ENVIRONMENT_REGION"),
 )
+
 
 def ingest_docs() -> None:
     loader = ReadTheDocsLoader(path="langchain-docs/")
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents) } documents")
     text_splitter = RecursiveCharacterTextSplitter(
-         chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
+        chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
     )
     documents = text_splitter.split_documents(documents=raw_documents)
     print(f"loaded {len(raw_documents)} documents")
@@ -27,4 +28,3 @@ def ingest_docs() -> None:
 
 if __name__ == "__main__":
     ingest_docs()
-
---
+++ main.py
@@ -0,0 +1,47 @@
+from typing import Set
+
+from backend.core import run_llm
+import streamlit as st
+from streamlit_chat import message
+
+
+
+st.header("Langchain Documentation Reader and Question Answerer")
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here")
+
+if 'chat_answer_history' not in st.session_state:
+    st.session_state['chat_answer_history'] = []
+
+if 'user_prompt_history' not in st.session_state:
+    st.session_state['user_prompt_history'] = []
+
+def create_source_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    source_list = list(source_urls)
+    source_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(source_list):
+        sources_string += f"{i+1}. {source}"
+    return sources_string
+
+
+if prompt:
+    with st.spinner("Generating response..."):
+        generated_response = run_llm(query=prompt)
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
+
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_source_string(sources)}"
+        )
+
+        st.session_state['user_prompt_history'].append(prompt)
+        st.session_state['chat_answer_history'].append(formatted_response)
+
+if st.session_state["chat_answer_history"]:
+    for user_query, generated_response in zip(st.session_state['user_prompt_history'], st.session_state['chat_answer_history']):
+        message(user_query, is_user=True)
+        message(generated_response)
